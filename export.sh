60d2232de99f983fd7d00ac0837b5f6533d6d520
Celso Dantas <celsodantas@gmail.com>
2018-10-29 19:29:23 +0100

    cleaning up commented code

--- export.sh
+++ export.sh
@@ -2,17 +2,3 @@ export ANT_ARGS='-logger org.apache.tools.ant.listener.AnsiColorLogger'
 export PATH="$HOME/.dotfiles/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 
-export GW2ORES_USER=homer
-export SPR_APP_URL='celsodantas-shopifyapps.fwd.wf'
-export VAGRANT_CWD='/Users/celsodantas/Code/vagrant'
-
-REACT_EDITOR=code
-
-export GOPATH=$HOME
-export SKIP_BUGSNAG=true
-export DONT_HELP_MY_COWORKERS_MAKE_EVIDENCE_BASED_DECISIONS=1
-export FUCK_YOU_STOP_SPYING_ME="FUCK YOU!"
-
-# makes GIT only start pagination (load LESS/VIM) when the content has more than 1 page
-# otherwise, it will just print in the terminal
-export LESS="$LESS -FRXK -+S"
