95a385739de5a6f0913c9f74fc97d19172dd7a94
Karim Mansour <karim.mansour@booking.com>
2018-11-25 20:06:24 +0100

    modify to my environment

--- alias.sh
+++ alias.sh
@@ -1,19 +1,29 @@
-alias glog="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
-alias gca="git add --all && git commit -a"
 alias e='subl .'
 alias c='cd'
-alias gl="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
-alias gs="git status"
-alias g='git'
-alias gd="git diff"
-alias gdd="git diff --cached"
-alias gdt="git difftool"
-alias ber="bundle exec rails"
 alias ctags_build="ctags -R -f .tags"
 alias tmux="TERM=screen-256color-bce tmux"
-alias bake="bundle exec rake"
-alias buby="bundle exec ruby"
-alias btest='bundle exec ruby -I.:test -e "ARGV.each{|f| require f}"'
-alias vs="cd ~/Code/vagrant && vagrant ssh"
 alias vu="vagrant up"
-alias vs="cd ~/Code/Ruby/vagrant && vagrant ssh"
+
+#git aliases
+alias g='git'
+alias gs='git status'
+alias gco='git checkout'
+alias gr='git rebase'
+alias gci='git commit'
+alias gpl='git pull'
+alias gpsh='git push'
+alias gd='git diff'
+alias gdt='git difftool'
+alias gdd='git diff --cached'
+alias gca="git add --all && git commit -a"
+alias gl="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
+alias glog="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
+
+# ping aliases
+alias ping='prettyping --nolegend'
+
+# fuzzy search (fzf)
+alias preview="fzf --preview 'bat --color \"always\" {}'"
+
+# tldr instead of man
+alias help='tldr'
--- export.sh
+++ export.sh
@@ -1,4 +1,5 @@
 export ANT_ARGS='-logger org.apache.tools.ant.listener.AnsiColorLogger'
 export PATH="$HOME/.dotfiles/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
-
+# add support for ctrl+o to open selected file in VS Code
+export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(code {})+abort'"
